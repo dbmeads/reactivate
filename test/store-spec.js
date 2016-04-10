fa3647560408678925d73901e99014a7fba116cf
David Meads <dbmeads@hotmail.com>
2016-04-10 09:04:19 -0400

    Add json schema support.

--- test/store-spec.js
+++ test/store-spec.js
@@ -3,13 +3,24 @@ import {expect} from 'chai';
 
 describe('store', () => {
 
-    var store = Reactivate.store('profile');
+    var store = Reactivate.store('profile').setSchema({
+        type: 'object',
+        properties: {
+            name: {
+                type: 'string'
+            }
+        },
+        additionalProperties: false,
+        required: ['name']
+    });
 
     it('should be immutable.', () => {
-        store.setState({name: 'Frank'});
-
-        store.getState().name = 'Jim';
+        store.setState({name: 'Frank'}).getState().name = 'Jim';
 
         expect(store.getState().name).to.eql('Frank');
+    });
+
+    it('should validate against json schema.', () => {
+        expect(() => store.setState({name: 'Don', extraProp: 'Hi!'})).to.throw(Error);
     });
 });
