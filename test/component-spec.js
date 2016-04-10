bb251f80629b3161777f944e1758886464e096d2
David Meads <dbmeads@hotmail.com>
2016-04-10 10:29:37 -0400

    Add test for our hacky React handling.

--- test/component-spec.js
+++ test/component-spec.js
@@ -2,4 +2,9 @@ import Reactivate from '../lib/index';
 import {expect} from 'chai';
 
 describe('store', () => {
+
+    it('should move React to global for now.', () => {
+        expect(React).to.exist;
+    });
+
 });
