7b43156e1030fc4b3acd53de41f9aac182b64b52
David Meads <dbmeads@hotmail.com>
2016-04-11 17:52:42 -0400

    Make sure store properly handles different types when immutable is enabled.

--- test/store-spec.js
+++ test/store-spec.js
@@ -1,17 +1,13 @@
 import Reactivate from '../lib/index';
 import {expect} from 'chai';
+import uuid from 'uuid';
 
 describe('store', () => {
 
-    var store = Reactivate.store('profile').setSchema({
-        type: 'object',
-        properties: {
-            name: {
-                type: 'string'
-            }
-        },
-        additionalProperties: false,
-        required: ['name']
+    var store;
+
+    beforeEach(() => {
+        store = Reactivate.store(uuid.v4());
     });
 
     it('should be immutable.', () => {
@@ -21,6 +17,33 @@ describe('store', () => {
     });
 
     it('should validate against JSON Schema.', () => {
+        store.setSchema({
+            type: 'object',
+            properties: {
+                name: {
+                    type: 'string'
+                }
+            },
+            additionalProperties: false,
+            required: ['name']
+        });
+
         expect(() => store.setState({name: 'Don', extraProp: 'Hi!'})).to.throw(Error);
+    });
+
+    it('should handle arrays', () => {
+        expect(store.setState([{name: 'Jim'}, {name: 'Bob'}]).getState().length).to.eql(2);
+    });
+
+    it('should handle strings', () => {
+        expect(store.setState('hi').getState()).to.equal('hi');
+    });
+
+    it('should handle numbers', () => {
+        expect(store.setState(1).getState()).to.equal(1);
+    });
+
+    it('should handle booleans', () => {
+        expect(store.setState(false).getState()).to.eql(false);
     });
 });
